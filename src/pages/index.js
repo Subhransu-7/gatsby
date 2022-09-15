7175fd9756d75cdab9f093bee30282b3b2285710
Subhransu Bidyasagar Kandi <47927545+Subhransu-7@users.noreply.github.com>
2022-09-15 18:17:03 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ const IndexPage = () => {
     <main>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+    <img src="https://i.pinimg.com/736x/2c/7f/69/2c7f69bb875315e9a3107e3cd1f904c4.jpg" alt="nature image">
     </main>
   )
 }
